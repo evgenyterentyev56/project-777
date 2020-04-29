32dadb7fe24091e9b5addc512c53e6e900cd7c4f
Evgeny <evgenyterentyev56@yandex.ru>
2020-04-30 00:50:27 +0400

    BMI - Calculator_v1.0.0_(30.04.2020)_(00:50). Расчеты bmi найдены

--- BmI-Calculator.py
+++ BmI-Calculator.py
@@ -2,28 +2,21 @@ from colorama import Fore, Back, Style
 # Передний план: ЧЕРНЫЙ, КРАСНЫЙ, ЗЕЛЕНЫЙ, ЖЕЛТЫЙ, СИНИЙ, МАГЕНТА, ЦИАНОВЫЙ, БЕЛЫЙ, СБРОС.
 # Назад: ЧЕРНЫЙ, КРАСНЫЙ, ЗЕЛЕНЫЙ, ЖЕЛТЫЙ, СИНИЙ, МАГЕНТА, ЦИАНОВЫЙ, БЕЛЫЙ, СБРОС.
 # Стиль: DIM, NORMAL, BRIGHT, RESET_ALL
-
 # Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
 # Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
 # Style: DIM, NORMAL, BRIGHT, RESET_ALL
-print(Style.BRIGHT)
+print(Style.NORMAL)
+print(Fore.GREEN)
 
-
-
-print('Введите ваши данные и нажмите клавишу "Enter":')
-
+print('Введите ваши данные:')
+print(Fore.BLUE)
 # Получаем данные от пользователя
-a = float(input("Введите ваш рост (См.)"))
-b = float(input("Введите ваш вес (кг.)"))
+# a = input("Введите ваш пол (м/ж)")
+b = float(input("Введите ваш рост (м./Прим. 1,72)"))
+c = float(input("Введите ваш вес (кг.)"))
+
+
 
 # Вычисления
-c = a * 2
-d = b / c * 100
-print("Результат " + str(d))
-
-if d >= "20":
-    print("Ok")
-
-
-
-
+bmi = c / b ** 2
+print(round(bmi))
