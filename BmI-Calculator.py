0ab20e47b2619c61b5c77a2d21d8f3090f90eed6
Evgeny <evgenyterentyev56@yandex.ru>
2020-04-30 16:08:44 +0400

    BMI - Calculator_v1.0.0_(29.04.2020)_(16:08).

--- BmI-Calculator.py
+++ BmI-Calculator.py
@@ -11,12 +11,16 @@ print(Fore.GREEN)
 print('Введите ваши данные:')
 print(Fore.BLUE)
 # Получаем данные от пользователя
-# a = input("Введите ваш пол (м/ж)")
-b = float(input("Введите ваш рост (м./Прим. 1,72)"))
-c = float(input("Введите ваш вес (кг.)"))
+
+b = float(input("Введите ваш рост (м./Пример. 1,72)"))
+c = int(input("Введите ваш вес (кг.)"))
 
 
 
 # Вычисления
 bmi = c / b ** 2
-print(round(bmi))
+bmi = str(round(bm))
+print("Результат " + bm)
+
+if bmi == "22":
+    print("Резульат ")
