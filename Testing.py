9f8cf9e7e70965f94b0a37a79bfb35c8bb63f608
Evgeny <evgenyterentyev56@yandex.ru>
2020-05-01 22:55:37 +0400

    Метео-v1_v1.0.0_(01.05.2020)_(22:55).

--- Testing.py
+++ Testing.py
@@ -1,11 +1,9 @@
 
 
-import telebot
+pogoda = 'Зима'
+time = 'День'
 
-bot = telebot.TeleBot("1253507210:AAFN8nyYPEKZhhU4JjTfFgID8cnVWAvr5rU")
-
-@bot.message_handler(content_types=['text'])
-def send_echo(message):
-	bot.reply_to(message, message.text)
-
-bot.polling( none_stop = True )
+if pogoda == 'Зима':
+	print("Сейчас холодно ")
+    if time == 'День':
+     print("ебать")
